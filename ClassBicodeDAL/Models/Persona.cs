2a5237d50bd62ecee6e93e541dce4d5202adec85
cristian18u <cristian18u@gmail.com>
2022-10-31 23:27:34 -0500

    Persona JsonIgnore

--- ClassBicodeDAL/Models/Persona.cs
+++ ClassBicodeDAL/Models/Persona.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace ClassBicodeDAL.Models
 {
@@ -14,8 +15,9 @@ namespace ClassBicodeDAL.Models
         public DateTime? FechaNacimiento { get; set; }
         public DateTime? FechaCreacion { get; set; }
         public DateTime? FechaActualizacion { get; set; }
-
+        [JsonIgnore]
         public virtual Documento? IdDocumentoNavigation { get; set; }
+        [JsonIgnore]
         public virtual Genero? IdGeneroNavigation { get; set; }
     }
 }
